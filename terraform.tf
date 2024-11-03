df6687e104c37dfb55f3ad6a88bb7490130a02f3
Rashid Ali Rabbani <rashidj0001@gmail.com>
2024-11-03 15:17:57 +0530

    Updated Terraform script for Azure VM deployment

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = var.backend_rg
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = "terraform.tfstate"
-  }
+  required_version = ">= 1.2.0"
 }
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  description = "Resource group for the VM"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for VM login"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for VM login"
+  type        = string
+  sensitive   = true
+}
