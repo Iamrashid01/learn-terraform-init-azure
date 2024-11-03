0316255ce04e604c0324ca05f1cf5b6f3b7cb983
Rashid Ali Rabbani <rashidj0001@gmail.com>
2024-11-03 14:53:19 +0530

    Initial commit of Azure Terraform setup

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.vm_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+module "azure-vm-instance" {
+  source              = "./modules/azure-vm-instance"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  vm_name             = var.vm_name
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  subnet_id           = azurerm_subnet.example.id
+}
